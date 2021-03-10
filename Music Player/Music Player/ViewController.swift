cc5f4ea7296030f535399f98bcb5dbfdf78bd5c1
DohyunKIm <rlaehgus1229@gmail.com>
2021-03-10 21:28:22 +0900

    [Add] XcodeProj

---
+++ Music Player/Music Player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Music Player
+//
+//  Created by DohyunKim on 2021/03/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
