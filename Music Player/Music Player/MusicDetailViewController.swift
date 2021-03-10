6f6fb0ca1afc2cbc7e083c2e931c4f56da301da2
DohyunKIm <rlaehgus1229@gmail.com>
2021-03-10 22:49:30 +0900

    [Add] MusicDetailViewController

---
+++ Music Player/Music Player/MusicDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MusicDetailViewController.swift
+//  Music Player
+//
+//  Created by DohyunKim on 2021/03/10.
+//
+
+import UIKit
+
+class MusicDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
