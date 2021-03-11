d67975ffd00c5ffef8d43719e6af787e8b1d2874
DohyunKIm <rlaehgus1229@gmail.com>
2021-03-11 11:07:41 +0900

    [Add] MusicModel

---
+++ Music Player/Music Player/MusicModel.swift
@@ -0,0 +1,14 @@
+//
+//  MusicModel.swift
+//  Music Player
+//
+//  Created by DohyunKim on 2021/03/11.
+//
+
+import Foundation
+
+    
+    let musicArr = ["파티피플","너무해","FedexxGirl"]
+    
+    var position = 0
+    
